78f4e6cebd84e3b624e95ec0a682e25ebc593894
admin <admin@gpw.com>
2019-06-12 20:17:08 +0800

    Merge branch 'master' of https://github.com/gpw1126/microservice
    # Please enter a commit message to explain why this merge is necessary,
    # especially if it merges an updated upstream into a topic branch.
    #
    # Lines starting with '#' will be ignored, and an empty message aborts
    # the commit.

---
+++ mycloud-configclient3355/src/main/java/com/gpw/springcloud/ConfigClient_3355_StartSpringCloudApp.java
@@ -0,0 +1,19 @@
+package com.gpw.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author: Gpw
+ * @Date: 2019/6/12
+ * @Description: com.gpw.springcloud
+ * @Version: 1.0
+ */
+@SpringBootApplication
+public class ConfigClient_3355_StartSpringCloudApp {
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(ConfigClient_3355_StartSpringCloudApp.class,args);
+    }
+}
